1d22674bf87873747fc644f12df717d69e270a4f
sebba93 <54692055+sebba93@users.noreply.github.com>
2022-04-17 16:43:04 -0400

    Analisis 2

--- Lab1.R
+++ Lab1.R
@@ -60,29 +60,70 @@ print(barraEdad)
 #------------------------------------------------------------------------#
 #------------------------------------------------------------------------#
 
-#FRECUENCIA DEL CUADRANTE.
+#FRECUENCIA DE LA MAMA Y CUADRANTE DE ESTA.
 
 #Tabla de Frecuencias
 contCuad <- xtabs(~ breast + breast_quad, data = data)
-contCuadTot <- marginSums(contCuad)
-contCuadFinal <- addmargins(contCuadTot,1)
-print(contCuadFinal)
+contCuadTot <- addmargins(contCuad)
+contCuadTot
+
 
 #Tabla de Porcentajes
-contCuadPor <- round(prop.table(contCuad), 6)*100
-contCuadPor <- addmargins(contEdadPor)
-print(contEdadPor)
+contCuadPor <- round(prop.table(contCuad),7)*100
+contCuadPorTot <- addmargins(contCuadPor)
+contCuadPorTot
 
-#Grafico de Barra
-contEdadDF<-as.data.frame(contEdad)
-barraEdad <- ggbarplot(contEdadDF,
-                       x = "age",
-                       y = "Freq",
-                       fill = c("pink","purple","pink",
-                                "purple","pink","purple"),
-                       title = "Cantidad de Pacientes en rango etáreo",
-                       xlab = "Edad",
-                       ylab = "Frecuencia")
 
-print(barraEdad)
+#Gráficos de Barras
+#Barra Segmentada
+contCuadDF <- as.data.frame(contCuad)
+bSegCuad <- ggplot(contCuadDF, aes(fill = breast, y = Freq, x = breast_quad))
+bSegCuad <- bSegCuad + geom_bar(position = "stack", stat = "identity")
+bSegCuad <- bSegCuad + labs(y = "Frecuencias") + ggtitle("Frecuencias por Cuadrante y Mama") + labs(x = "Cuadrantes")
+bSegCuad <- bSegCuad + theme_pubr()
+bSegCuad
+
+#Barra Agrupada
+contCuadDF <- as.data.frame(contCuad)
+bAgrCuad <- ggplot(contCuadDF, aes(fill = breast, y = Freq, x = breast_quad))
+bAgrCuad <- bAgrCuad + geom_bar(position = "dodge", stat = "identity")
+bAgrCuad <- bAgrCuad + labs(y = "Frecuencias") + ggtitle("Frecuencias por Cuadrante y Mama") + labs(x = "Cuadrantes")
+bAgrCuad <- bAgrCuad + theme_pubr()
+bAgrCuad
+
+#Barra Segmentada Estandarizada
+contCuadDF <- as.data.frame(contCuad)
+bEstCuad <- ggplot(contCuadDF, aes(fill = breast, y = Freq, x = breast_quad))
+bEstCuad <- bEstCuad + geom_bar(position = "fill", stat = "identity")
+bEstCuad <- bEstCuad + labs(y = "Frecuencias") + ggtitle("Frecuencias por Cuadrante y Mama") + labs(x = "Cuadrantes")
+bEstCuad <- bEstCuad + theme_pubr()
+bEstCuad
+
+#------------------------------------------------------------------------#
+#------------------------------------------------------------------------#
+#------------------------------------------------------------------------#
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
